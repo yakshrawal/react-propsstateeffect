baa079a956c7445beb7587bf879ff677a699f0b6
Yaksh Rawal <yakshrawal17@gmail.com>
2026-05-05 14:54:28 +0530

    .

--- Basics-React/src/App.jsx
+++ Basics-React/src/App.jsx
@@ -1,8 +1,18 @@
+// 1. Props (Properties) :
+// Props are used to pass data from a parent component → child component.
+// They are read-only inside the child.
+//  2. Use State : 
+// State is data that belongs to a component and can change over time.UseState lets functional components store and update state.
+// 3. UseEffect (Hook)
+// Used for side effects, like:
+// •	API calls
+// •	timers
+// •	logging
+// •	DOM updates 
+// Example : 
+
 import React, { useState, useEffect } from "react";
 
-// function Child(props) {
-//   return 
-// }
 
 function App(props) {
   const [count, setCount] = useState(0);
